c212f03cbccd88a5b12e0b2d442cc213d2853774
Kitadeve <kitadevturtle@gmail.com>
2025-04-16 19:18:55 +0200

    Mission accomplie

---
+++ js/global.js
@@ -0,0 +1,35 @@
+function updateHeaderHeight() {
+    const header = document.querySelector("header");
+    const root = document.documentElement;
+
+    // Set the header height as a CSS variable
+    root.style.setProperty("--header-height", `${header.offsetHeight}px`);
+}
+
+function menuMobile () {
+    const btn = document.querySelector(".burger");
+    const header = document.querySelector("header");
+    const links = document.querySelectorAll(".burger-overlap a");
+    const body = document.body;
+  
+    btn.addEventListener("click", () => {
+        header.classList.toggle("show-overlap");
+        body.classList.toggle("no-scroll");
+      });
+
+      links.forEach(link => {
+        link.addEventListener("click", () =>{
+          header.classList.remove("show-overlap");
+          body.classList.remove("no-scroll");
+      });
+    })
+   
+    
+  }
+
+    window.addEventListener("load", updateHeaderHeight);
+    window.addEventListener("resize", updateHeaderHeight);
+
+  menuMobile();
+
+  
