c1da093cf0a087beabde20987db03092033f5de1
Kitadeve <kitadevturtle@gmail.com>
2025-05-16 16:19:26 +0200

    debut de travail potentiellement dangeureux

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,52 +1,58 @@
 const form = document.querySelector("form");
 const message = document.querySelector(".message");
-console.log('hello');
 let inputLastNameIsValid = false;
 let inputFirstNameIsValid = false;
 let inputEmailIsValid = false;
-let inputSelectIsValid = false;
+// let inputSelectIsValid = false;
+
+// Nettoie les entrées utilisateur
+function sanitizeInput(input) {
+  return input.replace(/</g, "&lt;")
+              .replace(/>/g, "&gt;")
+              .replace(/["']/g, "")
+              .replace(/&/g, "&amp;");
+}
+
+// Autorise uniquement lettres, espaces, tirets et apostrophes
+function isValidName(name) {
+  return /^[a-zA-ZÀ-ÿ '-]+$/.test(name);
+}
 
 form.addEventListener('submit', function(e){
-    e.preventDefault()
-    
-    //1. accés à l'élément input name
-    const lname = form.lname.value;
-    const fname = form.fname.value;
-    const email = form.email.value;
-    const select = form.subject;
+    e.preventDefault();
 
-    //2. Vérifier qu le champ est valide
+    // Nettoyage des entrées
+    const lastName = sanitizeInput(form.lastName.value.trim());
+    const firstName = sanitizeInput(form.fisrtName.value.trim());
+    const email = sanitizeInput(form.email.value.trim());
+    // const select = sanitizeInput(form.subject.value);
 
+    // Réinitialisation des flags
+    inputLastNameIsValid = false;
+    inputFirstNameIsValid = false;
+    inputEmailIsValid = false;
 
-    if (lname != ""){
-        inputLastNameIsValid = true
+    // Validation des champs
+    if (lastName !== "" && isValidName(lastName)) {
+        inputLastNameIsValid = true;
     }
 
-    if (fname != ""){
-        inputFirstNameIsValid = true
+    if (firstName !== "" && isValidName(firstName)) {
+        inputFirstNameIsValid = true;
     }
 
     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
         inputEmailIsValid = true;
     }
 
-    if (select != "") {
-        inputSelectIsValid = true
+    if (inputLastNameIsValid && inputFirstNameIsValid && inputEmailIsValid && inputSelectIsValid) {
+        message.innerText = "Formulaire envoyé";
+        message.classList.add("message-succes");
+        message.classList.remove("message-error");
+        // Ici, tu peux envoyer le formulaire via AJAX si besoin
+    } else {
+        message.innerText = "Formulaire invalide";
+        message.classList.add("message-error");
+        message.classList.remove("message-succes");
     }
-
-
-    if (inputNameIsValid && inputEmailIsValid && inputSelectIsValid){
-        message.innerText = "Formulaire envoyé"
-        message.classList.add("message-succes")
-        message.classList.remove("message-error")
-    }
-
-    else {
-        message.innerText = "Formulaire invalide"
-        message.classList.add("message-error")
-        message.classList.remove("message-succes")
-    }
-
 });
-
-console.log('hello');
