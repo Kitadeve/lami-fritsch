4f52b408db53eb2d90e6ecc952062b51144985a2
Kitadeve <kitadevturtle@gmail.com>
2025-05-26 19:52:48 +0200

    remis le mdp sur admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,8 +1,8 @@
-  // const motDePasse = prompt("Mot de passe pour accéder à l'administration :");
-  // if (motDePasse !== "1234") {
-  //   alert("Mot de passe incorrect !");
-  //   window.location.href = "index.html"; // ou une autre page
-  // }
+  const motDePasse = prompt("Mot de passe pour accéder à l'administration :");
+  if (motDePasse !== "1234") {
+    alert("Mot de passe incorrect !");
+    window.location.href = "index.html"; // ou une autre page
+  }
 
 function sanitizeInput(input) {
   return input.replace(/</g, "&lt;")
