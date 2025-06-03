974d19471f4e3250bf79980704b7edbe9fdb89d5
Kitadeve <kitadevturtle@gmail.com>
2025-06-03 15:13:08 +0200

    explication pdj.js

--- assets/js/pdj.js
+++ assets/js/pdj.js
@@ -1,13 +1,24 @@
   window.addEventListener("DOMContentLoaded", () => {
     const platsDuJour = JSON.parse(localStorage.getItem("platsDuJour") || "{}");
+    const cards = document.querySelectorAll(".card-jour");
 
-    document.querySelectorAll(".card-jour").forEach(card => {
-      const jourEl = card.querySelector("h3.jour");
-      const jour = jourEl.textContent.trim();
-      
+    /* on parcourt chaque card-jour (il y en a trois dans ce cas), 
+    pour chacune d'entre elle on va chercher le jour, en ciblant le text content
+    de l'élément h3 de classe jour*/
+    cards.forEach(card => {
+      const jour = card.querySelector("h3.jour").textContent.trim();
+      /* si le le jour existe dans l'objet platDuJour, 
+      stocké dans le localStorage, on remplace le contenu de la 
+      frame (entrée + trait + plat) par le html écrit ici et les valeurs
+      récupérée dans le localStorage.  */
       if (platsDuJour[jour]) {
-        const { entree, plat } = platsDuJour[jour];
-        const frame = card.querySelector(".frame");
+
+        // //destructuration d'objet, créé 2 variables, en une seule ligne. J'ai préféré l'écrire en détails en dessous carr mieux maitrisé
+        // const { entree, plat } = platsDuJour[jour];
+
+        const entree = platsDuJour[jour].entree; 
+        const plat = platsDuJour[jour].plat;
+        const frame = card.querySelector(".frame");        
         frame.innerHTML = `
           <div class="entree">${entree}</div>
           <div class="trait"></div>
@@ -17,7 +28,7 @@
     });
   });
 
-// version en fetch, pour chercher le menu écrit en dur dans le json et similer la recherche dans une bdd//
+// version en fetch, pour chercher le menu écrit en dur dans le json et simuler la recherche dans une bdd//
 
 // fetch('./assets/json/menus-jour.json')
 //   .then(response => response.json())
